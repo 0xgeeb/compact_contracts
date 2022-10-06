fb98722eb38f741d61ba39a4e7dd86bdaeed11dd
0xgeeb <0xgeeb@gmail.com>
2022-10-06 18:03:41 -0400

    added createPair() function on ccUniswapV2Factory

--- src/ccUniswap/ccUniswapV2Factory.sol
+++ src/ccUniswap/ccUniswapV2Factory.sol
@@ -1,6 +1,32 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "./ccUniswapV2Pair.sol";
+
 contract ccUniswapV2Factory {
+
+  mapping(address => mapping(address => address)) public getPair;
+  address[] public allPairs;
+
+  function createPair(address _tokenA, address _tokenB) public returns (address _pair) {
+    require(_tokenA != _tokenB, "identical addresses");
+    require(getPair[_tokenA][_tokenB] == address(0), "pair exists");
+    bytes memory _bytecode = type(ccUniswapV2Pair).creationCode;
+    bytes32 salt = keccak256(abi.encodePacked(_tokenA, _tokenB));
+    assembly {
+      _pair := create2(0, add(_bytecode, 32), mload(_bytecode), salt)
+    }
+    ccUniswapV2Pair(_pair).initialize(_tokenA, _tokenB);
+    getPair[_tokenA][_tokenB] = _pair;
+    getPair[_tokenB][_tokenA] = _pair;
+    allPairs.push(_pair);
+  }
+
+
+
+
+
+
+
   
 }
--- src/ccUniswap/ccUniswapV2Pair.sol
+++ src/ccUniswap/ccUniswapV2Pair.sol
@@ -2,5 +2,19 @@
 pragma solidity ^0.8.9;
 
 contract ccUniswapV2Pair {
+
+  address public factory;
+  address public token0;
+  address public token1;
+
+  constructor() {
+    factory = msg.sender;
+  }
+
+  function initialize(address _token0, address _token1) public {
+    require(msg.sender == factory, "forbidden");
+    token0 = _token0;
+    token1 = _token1;
+  }
   
 }
