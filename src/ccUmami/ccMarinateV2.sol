f8c9006f818db7dad75e75f469a090d4f4ade358
0xgeeb <0xgeeb@gmail.com>
2022-10-25 08:34:58 -0400

    added umami token

--- src/ccUmami/ccMarinateV2.sol
+++ src/ccUmami/ccMarinateV2.sol
@@ -1,7 +1,21 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.9;
 
+import "../../lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+
 contract ccMarinateV2 {
+
+  using EnumerableSet for EnumerableSet.AddressSet;
+
+  mapping(address => uint256) public totalTokenRewardsPerStake;
+  mapping(address => mapping(address => uint256)) public paidTokenRewardsPerStake;
+  mapping(address => mapping(address => uint256)) public toBePaid;
+  EnumerableSet.AddressSet private rewardTokens;
+
+  constructor(address) {
+
+  }
 
 
   
