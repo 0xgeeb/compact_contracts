3e7c7cf352df271e7c05f89d5c2e7e5300884a5b
0xgeeb <0xgeeb@gmail.com>
2022-10-26 09:23:58 -0400

    worked on ccMarinateV2

--- src/ccUmami/ccMarinateV2.sol
+++ src/ccUmami/ccMarinateV2.sol
@@ -3,8 +3,9 @@ pragma solidity ^0.8.9;
 
 import "../../lib/openzeppelin-contracts/contracts/utils/structs/EnumerableSet.sol";
 import "../../lib/openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
+import "../../lib/openzeppelin-contracts/contracts/token/ERC20/ERC20.sol";
 
-contract ccMarinateV2 {
+contract ccMarinateV2 is ERC20 {
 
   using EnumerableSet for EnumerableSet.AddressSet;
 
@@ -12,9 +13,14 @@ contract ccMarinateV2 {
   mapping(address => mapping(address => uint256)) public paidTokenRewardsPerStake;
   mapping(address => mapping(address => uint256)) public toBePaid;
   EnumerableSet.AddressSet private rewardTokens;
+  address public immutable umami;
 
-  constructor(address) {
+  constructor(address) ERC20() {
 
+  }
+
+  function stake(uint256 _amount) public {
+    IERC20(umami).transferFrom(msg.sender, address(this), _amount);
   }
 
 
